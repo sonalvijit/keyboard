5190cf7eafc3e18885657ec74f3ed42c86c16fe4
svjt <josephtoppo.sxd@gmail.com>
2025-06-20 00:29:31 +0530

    Added a basic Key binding highlight for just letters

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 document.addEventListener('keydown', (event) => {
-     const key = event.normal;
-     if (key === 'SHIFT') {
-          document.querySelectorAll('.key').forEach(KeyEl => {
-               const shifted = KeyEl.getAttribute('data-shift');
-               if (shifted) KeyEl.textContent = shifted;
-          });
-     }
+  const key = event.key;
+  
+  if (key === 'Shift') {
+    // Show shifted characters
+    document.querySelectorAll('.key').forEach(keyEl => {
+      const shifted = keyEl.getAttribute('data-shift');
+      if (shifted) keyEl.textContent = shifted;
+    });
+  }
 
-     // Blink the key
-     // const matchKey = key.length === 1 ? key.toUpperCase() : key;
-     const keyDiv = document.querySelector(`.key[data-key="${matchKey}"]`);
-     if (keyDiv) {
-          keyDiv.classList.add('active');
-          setTimeout(() => keyDiv.classList.remove('active'), 200);
-     }
+  // Blink the key
+//   const matchKey = key.length === 1 ? key.toUpperCase() : key;
+//   console.log(matchKey);
+  const keyDiv = document.querySelector(`.key[data-key="${key}"]`);
+  if (keyDiv) {
+    keyDiv.classList.add('active');
+    setTimeout(() => keyDiv.classList.remove('active'), 200);
+  }
 });
 
 document.addEventListener('keyup', (event) => {
-     if (event.key === 'Shift') {
-          // Revert to normal characters
-          document.querySelectorAll('.key').forEach(keyEl => {
-               const normal = keyEl.getAttribute('data-normal');
-               if (normal) keyEl.textContent = normal;
-          });
-     }
-})
+  if (event.key === 'Shift') {
+    // Revert to normal characters
+    document.querySelectorAll('.key').forEach(keyEl => {
+      const normal = keyEl.getAttribute('data-normal');
+      if (normal) keyEl.textContent = normal;
+    });
+  }
+});
